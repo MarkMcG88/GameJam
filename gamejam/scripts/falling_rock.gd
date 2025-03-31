76dfc582c27329fbba1eb78d25b00c47a117b245
Mark <mcgarvie13@gmail.com>
2025-03-31 21:15:01 +0100

    New zone - includes rocks

---
+++ gamejam/scripts/falling_rock.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+@export var fall_speed: float = 320
+@export var is_falling: bool = false
+
+@onready var killbox = $Killbox
+@onready var trigger_area = $TriggerArea
+@onready var timer = $SelfDestructTimer
+
+var checkpoint_manager
+var player
+var startPosition
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	checkpoint_manager = get_parent().get_node("CheckpointManager")
+	player = get_parent().get_node("Player")
+	startPosition = position
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	if is_falling:
+		global_position.y += fall_speed * delta
+		fall_speed += 10
+	
+func _on_trigger_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("player detected")
+		is_falling = true
+		timer.start()
+
+
+func _on_killbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("player hit")
+		timer.stop()
+		position = startPosition
+		is_falling = false
+		fall_speed = 320
+		AudioController.play_falling()
+		killPlayer()
+		
+func killPlayer():
+	player.position = checkpoint_manager.last_location
+
+
+func _on_self_destruct_timer_timeout() -> void:
+	print("rock destroyed")
+	queue_free()
