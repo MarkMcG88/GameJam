864aa5e0bfb4d1652b6dfef878c5b818884bb29a
Mark <mcgarvie13@gmail.com>
2025-03-31 18:39:54 +0100

    Fixed bug

--- gamejam/scripts/canvas_layer.gd
+++
@@ -1,5 +0,0 @@
-extends CanvasLayer
-
-func _process(delta):
-	var viewport_size = get_viewport_rect().size
-	position = Vector2(viewport_size.x - 120, 20)  # Adjust as needed
--- gamejam/scripts/game.gd
+++ gamejam/scripts/game.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var player = get_node("Player")  # Path to Player node
 @onready var canvas_layer = get_node("CanvasLayer")  # Get Camera2D node from Player
 @onready var bone_control = canvas_layer.get_node("BoneControl")  # Get Camera2D node from Player
-@onready var bones_collected = bone_control.get_node("BonesCollected")  # Get BonesCollected node from Camera2D
+@onready var bones_collected = get_node("BonesCollected")  # Get BonesCollected node from Camera2D
 
 func _ready():	
 	player.bones_updated.connect(bones_collected.update_bones_list)
