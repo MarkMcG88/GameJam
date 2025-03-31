c5e718fbd4eef32e91934045359b4e2941964999
Mark <mcgarvie13@gmail.com>
2025-03-31 19:43:57 +0100

    bone collection is now tracked

--- gamejam/scripts/bones_collected.gd
+++ gamejam/scripts/bones_collected.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-func _process(delta):
-	var viewport_size = get_viewport_rect().size
-	position = Vector2(viewport_size.x - 120, 20) 
-
 func update_bones_list(bones: Array):
 
 	for child in get_children():
@@ -13,14 +9,13 @@ func update_bones_list(bones: Array):
 	
 	print("update bones")
 	
-
 	for bone in bones:
 		var sprite = TextureRect.new()
 		sprite.texture = bone.sprite
 		sprite.custom_minimum_size = Vector2(16, 16) 
-		sprite.scale = Vector2(0.25, 0.25)
+		sprite.scale = Vector2(0.4, 0.4)
 		sprite.position = Vector2(x_offset, 0) 
-		x_offset += 40 
+		x_offset -= 80 
 		add_child(sprite)
 		
 		print(get_children())
--- gamejam/scripts/game.gd
+++ gamejam/scripts/game.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 @onready var player = get_node("Player")
-@onready var camera = player.get_node("Camera2D")
-@onready var bones_collected = camera.get_node("BonesCollected")  
+@onready var hud = get_node("Hud")
+@onready var control = hud.get_node("Control")
+@onready var bones_collected = control.get_node("BonesCollected")  
 
 func _ready():	
 	player.bones_updated.connect(bones_collected.update_bones_list)
